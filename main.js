2f87dcdf805c156cc004598b432414fae9da909e
mdiazc <spalduing12@gmail.com>
2022-03-23 15:17:25 -0500

    Added: Classes

--- main.js
+++ main.js
@@ -29,3 +29,51 @@ Student.prototype.aprovarCurso = function (nuevoCurso) {
 };
 
 const juanita = new Student('Juanita', 25, ['Scope y Closure']);
+
+// Class Syntax Prototypes (Syntactic Sugar)
+
+class Student2 {
+  constructor(name, age, cursosAprobados) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+
+    // this.aprovarCurso = function (nuevoCurso) {
+    //   this.cursosAprobados.push(nuevoCurso);
+    // };
+  }
+
+  aprovarCurso(nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+  }
+}
+
+const pablito = new Student2('Pablito', 28, ['Curso práctico de javascript']);
+
+class Student3 {
+  constructor({
+    name = '',
+    age = 0,
+    facebook = '',
+    instagram = '',
+    email = '',
+    cursosAprobados = [],
+  }) {
+    this.name = name;
+    this.age = age;
+    this.facebook = facebook;
+    this.instagram = instagram;
+    this.email = email;
+    this.cursosAprobados = cursosAprobados;
+  }
+
+  aprovarCurso(nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+  }
+}
+
+const pedrito = new Student3({
+  cursosAprobados: ['Curso práctico de javascript'],
+  name: 'Pablito',
+  age: 28,
+});
