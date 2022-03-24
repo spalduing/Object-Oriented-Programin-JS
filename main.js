9e1d5c1544b1a7c354e2eec715e9343052cdea46
mdiazc <spalduing12@gmail.com>
2022-03-24 15:06:00 -0500

    Added: Encapsulation

--- main.js
+++ main.js
@@ -169,3 +169,29 @@ const escuelaVideojuegos = new Learning({
   name: 'Escuela data Science',
   courses: ['Curso de Unrealengine', 'Curso de Unitiy en 3D'],
 });
+
+class Course {
+  constructor({ name, classes = [] }) {
+    this._name = name;
+    this.classes = classes;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (nuevoCurso === 'Curso Maldito de Programacion Basica') {
+      console.error('Web...no');
+    } else {
+      this._name = newName;
+    }
+  }
+}
+
+const cursoProgBasica = new Course({
+  name: 'Curso gratis de programación básica',
+});
+
+// cursoProgBasica.name // calls the get name()
+// cursoProgrBasica.name = "lalalalalalala...la // calls the set name(newName)"
